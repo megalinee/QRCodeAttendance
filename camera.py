4ddfacdbe110ae7a47bea0d33106c6a038e70f40
Murad <agariomadman@gmail.com>
2022-08-05 20:07:39 -0500

    Fixed camera executable

--- camera.py
+++ camera.py
@@ -1,12 +1,11 @@
-import cv2
-import json
-from os.path import exists
 from datetime import date
-from pyzbar import pyzbar
+from os.path import exists
+import json
+import cv2
 
 pathToJSON = "./data.json"
 
-dateString = date.today().strftime("%m-%d-%Y");
+dateString = date.today().strftime("%m-%d-%Y")
 
 if not exists(pathToJSON):
     f = open(pathToJSON, "a")
@@ -17,64 +16,68 @@ vid = cv2.VideoCapture(0)
 
 qrDecoder = cv2.QRCodeDetector()
 
-font                   = cv2.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (20,30)
-fontScale              = 1
-fontColor              = (255,255,255)
-thickness              = 3
-lineType               = 2
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (20, 30)
+fontScale = 1
+fontColor = (255, 255, 255)
+thickness = 3
+lineType = 2
 
 lastDetected = 0
 
+
 def add_date(filename=pathToJSON):
-    with open(filename,'r+') as file:
+    with open(filename, 'r+') as file:
         file_data = json.load(file)
         for member in file_data["members"]:
             member["days-attended"][dateString] = False
         file.seek(0)
-        json.dump(file_data, file, indent = 4)
+        json.dump(file_data, file, indent=4)
+
 
 add_date()
 
+
 def change_attendance(idnum, filename=pathToJSON):
     name = None
-    with open(filename,'r+') as file:
+    with open(filename, 'r+') as file:
         file_data = json.load(file)
         for member in file_data["members"]:
             if member["id"] == idnum:
                 name = member["name"]
                 member["days-attended"][dateString] = True
         file.seek(0)
-        updatedJSON = json.dumps(file_data, indent = 4)
-    with open(filename,'w') as file:
+        updatedJSON = json.dumps(file_data, indent=4)
+    with open(filename, 'w') as file:
         file.write(updatedJSON)
         file.close()
     return name
+
 
 while(True):
     lastDetected += 1
 
     ret, frame = vid.read()
-  
+
     msg = ""
-    scanner = pyzbar.decode(frame)
-    if len(scanner) > 0 and lastDetected > 50:
-        data = scanner[0].data.decode('utf-8')
+    data, points, _ = qrDecoder.detectAndDecode(frame)
+    if data and lastDetected > 50:
         lastDetected = 0
-        value = int(format(data))
+        value = int(data)
         user = change_attendance(value)
         if user is not None:
             msg = "Welcome " + user + "!"
         else:
-            msg  = "Invalid User ID: " + str(value)
+            msg = "Invalid User ID: " + str(value)
         recognizedFrame = frame.copy()
-        cv2.putText(recognizedFrame, msg, bottomLeftCornerOfText, font, fontScale,fontColor,thickness,lineType)
+        cv2.putText(recognizedFrame, msg, bottomLeftCornerOfText,
+                    font, fontScale, fontColor, thickness, lineType)
         cv2.imshow('poop', recognizedFrame)
-        
+
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-  
+
 
 vid.release()
 cv2.destroyAllWindows()
